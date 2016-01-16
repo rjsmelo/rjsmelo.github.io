8a36f821a98915f4ce9146160c26a49cba02200b
Ricardo Melo <rjsmelo@gmail.com>
2016-01-16 18:09:40 +0000

    Automate publish process

--- publish.sh
+++ publish.sh
@@ -7,7 +7,22 @@
 sculpin generate --env=prod
 if [ $? -ne 0 ]; then echo "Could not generate the site"; exit 1; fi
 
-# Add --delete right before "output_prod" to have rsync remove files that are
-# deleted locally from the destination too. See README.md for an example.
-rsync -avze 'ssh -p 4668' output_prod/ username@yoursculpinsite:public_html
-if [ $? -ne 0 ]; then echo "Could not publish the site"; exit 1; fi
+git stash
+git checkout master
+
+cp -R output_prod/* .
+git add *
+git status
+
+echo -n "Do you want to commit changes (y/n): "
+read COMMIT 
+if [ "_$COMMIT" != "_y" ] ; then
+	echo "Aborted by user command, please commit manually"
+fi
+
+git commit
+git push origin --all
+
+git checkout source
+git stash pop
+
